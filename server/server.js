02001ab9fd8f60933d797ff1ba05dfdf0d229374
Leran Firer <leranf@gmail.com>
2016-06-23 17:43:24 -0700

    adds script for google places api for geosuggest

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ import Helmet from 'react-helmet';
 import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
 import posts from './routes/post.routes';
+import search from './routes/search.routes';
 import dummyData from './dummyData';
 import serverConfig from './config';
 
@@ -52,7 +53,7 @@ app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist')));
-app.use('/api', posts);
+app.use('/api', posts, search);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
@@ -75,6 +76,7 @@ const renderFullPage = (html, initialState) => {
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
         <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+        <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCmztPTewNHhj0msWke8rby91SNULIzhvs&libraries=places'></script>
       </head>
       <body>
         <div id="root">${html}</div>
