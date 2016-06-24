656c3a6abfe51c5a8f4c3a58baea806e6b95c0e7
Leran Firer <leranf@gmail.com>
2016-06-23 17:41:47 -0700

    adds reducers for search and results

--- client/reducers.js
+++ client/reducers.js
@@ -7,10 +7,14 @@ import { combineReducers } from 'redux';
 import app from './modules/App/AppReducer';
 import posts from './modules/Post/PostReducer';
 import intl from './modules/Intl/IntlReducer';
+import results from './modules/Results/ResultsReducer';
+import search from './modules/Search/SearchReducer';
 
 // Combine all reducers into one root reducer
 export default combineReducers({
   app,
   posts,
   intl,
+  results,
+  search,
 });
