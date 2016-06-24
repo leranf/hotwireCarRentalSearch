8189b938c7317c2b3e8fd8cba13c149f4233df3d
Leran Firer <leranf@gmail.com>
2016-06-24 13:23:25 -0700

    changes form to a table, inclues calendar and time pickers

--- client/modules/Search/components/Form.js
+++ client/modules/Search/components/Form.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Geosuggest from 'react-geosuggest';
-import Submit from './Submit';
+import Calendar from 'react-input-calendar';
+import TimeSelect from 'react-time-select';
+import 'moment-range';
 import {setPickUpLocation, setPickUpDate, setDropOffDate, setPickUpTime, setDropOffTime} from '../SearchActions';
 
 const mapDispatchToProps = dispatch => ({
@@ -12,19 +14,24 @@ const mapDispatchToProps = dispatch => ({
   changeDropOffTime: time => dispatch(setDropOffTime(time)),
 });
 
-const Form = ({changePickUpLocation, changePickUpDate, changeDropOffDate, changePickUpTime, changeDropOffTime}) => (
+const Form = ({ changePickUpLocation, changePickUpDate, changeDropOffDate, changePickUpTime, changeDropOffTime }) => (
   <div className="container">
     <h3>Search Hotwire Cars!!</h3>
-    <label>Location</label>
-    <Geosuggest onSuggestSelect={suggest => changePickUpLocation(suggest.label)}/>
-    <label>Pick up date</label>
-    <input onChange={e => changePickUpDate(e.target.value)}/>
-    <label>Pick up time</label>
-    <input onChange={e => changePickUpTime(e.target.value)}/>
-    <label>Drop off date</label>
-    <input onChange={e => changeDropOffDate(e.target.value)}/>
-    <label>Drop off time</label>
-    <input onChange={e => changeDropOffTime(e.target.value)}/>
+    <table>
+      <tr>Location</tr>
+      <tr>
+        <Geosuggest onSuggestSelect={suggest => changePickUpLocation(suggest.label)} />
+      </tr>
+    </table>
+    <tr>Pick up date</tr>
+    <tr>
+      <td><Calendar format="MM/DD/YYYY" computableFormat="MM/DD/YYYY" onChange={e => changePickUpDate(e)}/></td>
+      <td><TimeSelect onChange={e => changePickUpTime(e.toTimeString().slice(0, 5))}/></td>
+    </tr>
+    <tr>
+      <td><Calendar format="MM/DD/YYYY" computableFormat="MM/DD/YYYY" onChange={e => changeDropOffDate(e)}/></td>
+      <td><TimeSelect onChange={e => changeDropOffTime(e.toTimeString().slice(0, 5))}/></td>
+    </tr>
   </div>
 );
 
