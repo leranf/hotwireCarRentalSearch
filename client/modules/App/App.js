3e6cadeb18d19e05081ed8ba13bb1dc8df7c9dae
Leran Firer <leranf@gmail.com>
2016-06-24 17:26:12 -0700

    removes footer

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -8,7 +8,6 @@ import styles from './App.css';
 import Helmet from 'react-helmet';
 import DevTools from './components/DevTools';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 
 // Import Actions
 import { toggleAddPost } from './AppActions';
@@ -56,7 +55,6 @@ export class App extends Component {
           <div className={styles.container}>
             {this.props.children}
           </div>
-          <Footer />
         </div>
       </div>
     );
