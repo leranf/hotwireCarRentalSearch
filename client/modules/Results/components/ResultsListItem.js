c7d7666f097d920185ccdb00fd97534911bf89e9
Leran Firer <leranf@gmail.com>
2016-06-24 17:29:17 -0700

    organizes result item data

--- client/modules/Results/components/ResultsListItem.js
+++ client/modules/Results/components/ResultsListItem.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styles from './ResultsListItem';
 
 const mapStateToProps = state => ({
   carTypes: state.results.carTypes,
 });
 
-const ResultsListItem = ({ SubTotal, TaxesAndFees, TotalPrice, MileageDescription }) => (
-  <div>
-    <td>Mileage: {MileageDescription}</td>
-    <td>SubTotal: {SubTotal}</td>
-    <td>Taxes: {TaxesAndFees}</td>
-    <td>Total: {TotalPrice}</td>
+const ResultsListItem = ({ carTypes, CarTypeCode, DailyRate, TotalPrice, MileageDescription }) => (
+  <div style={{ padding: '10px' }}>
+    <table className={styles.resultsListItem} style={{ border: '1px solid', width: '500px' }}>
+      <tr>
+        <td style={{ float: 'left', 'font-weight': 'bold', 'font-size': 'larger' }}>{carTypes[CarTypeCode].CarTypeName}</td>
+        <td style={{ float: 'right', 'font-weight': 'bold', 'font-size': 'larger', color: 'red' }}>${DailyRate.split('.')[0]}</td>
+      </tr>
+      <tr>
+        <td style={{ float: 'left' }}>{carTypes[CarTypeCode].PossibleModels}</td>
+        <td style={{ float: 'right', 'font-size': 'small' }}>and up, per day</td>
+      </tr>
+      <tr>
+        <td style={{ float: 'left' }}>{MileageDescription} miles</td>
+        <td style={{ float: 'right' }}>${TotalPrice.split('.')[0]} total</td>
+      </tr>
+    </table>
   </div>
 );
 
 ResultsListItem.propTypes = {
-  SubTotal: React.PropTypes.string.isRequired,
+  carTypes: React.PropTypes.object.isRequired,
+  CarTypeCode: React.PropTypes.string.isRequired,
   TaxesAndFees: React.PropTypes.string.isRequired,
   TotalPrice: React.PropTypes.string.isRequired,
   MileageDescription: React.PropTypes.string.isRequired,
